1f3ad980101dd08cea50fa935f3b464d181261fc
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2023-08-12 06:05:42 +0900

    [level 2] Title: 주식가격, Time: 11.34 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/README.md
+++ 프로그래머스/lv2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 66.6 MB, 시간: 9.41 ms
+메모리: 60.6 MB, 시간: 11.34 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -3,12 +3,10 @@ class Solution {
         int[] answer = new int[prices.length];
         
         for (int i = 0; i < prices.length; i++) {
-            int sec = 0;
             for (int j = i + 1; j < prices.length; j++) {
-                sec++;
+                answer[i]++;
                 if (prices[i] > prices[j]) break;
             }
-            answer[i] = sec;
         }
         
         return answer;
