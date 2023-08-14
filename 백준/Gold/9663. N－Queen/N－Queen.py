86795bd2dcbde44e1a06682a5c600d8ebcbfd3e1
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2023-08-15 02:51:13 +0900

    [Gold IV] Title: N-Queen, Time: 23076 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+result = 0
+
+is_used_1 = [False] * n
+is_used_2 = [False] * (2 * n - 1)
+is_used_3 = [False] * (2 * n - 1)
+
+def dfs(row):
+  global result
+  
+  if row == n:
+    result += 1
+    return
+
+  for col in range(n):
+    if is_used_1[col] or is_used_2[row + col] or is_used_3[row - col + n - 1]:
+      continue
+    is_used_1[col] = True
+    is_used_2[row + col] = True
+    is_used_3[row - col + n - 1] = True
+    dfs(row + 1)
+    is_used_1[col] = False
+    is_used_2[row + col] = False
+    is_used_3[row - col + n - 1] = False
+
+dfs(0)
+print(result)
