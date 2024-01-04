0686eeae646e524778f63c14ae880594ef02da59
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-01-04 10:40:49 +0900

    [Silver III] Title: 소수 구하기, Time: 1016 ms, Memory: 112336 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -2,9 +2,12 @@ def is_prime(x):
     for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
             return False
-    return x != 1
+    return True
 
 m, n = map(int, input().split())
+if m == 1:
+    m += 1
+    
 for x in range(m, n + 1):
     if is_prime(x):
         print(x)
