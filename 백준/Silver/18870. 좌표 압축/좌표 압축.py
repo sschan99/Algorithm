9acaa80555a96b4587a784b7879b6c8daaea712a
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-01-20 20:04:07 +0900

    [Silver II] Title: 좌표 압축, Time: 1784 ms, Memory: 143396 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,16 +1,11 @@
-import heapq
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-nums = sorted(enumerate(map(int, input().split())), key=lambda x: x[1])
+nums = list(map(int, input().split()))
 
-prev = float('-inf')
-asc = -1
-output = [-1] * n
-for i, x in nums:
-    if x > prev:
-        asc += 1
-    output[i] = asc
-    prev = x
-print(*output)
+d = {}
+for i, x in enumerate(sorted(set(nums))):
+    d[x] = i
+
+print(*map(lambda x: d[x], nums))
