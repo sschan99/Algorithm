9db04a3da9ec876de96d57103b8c93c7b528d26d
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-01-21 16:20:00 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 400 ms, Memory: 39660 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -5,8 +5,7 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(n)]
-dist = [[float('inf')] * m for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
+dist = [[-1] * m for _ in range(n)]
 
 def find_goal():
     for i in range(n):
@@ -17,7 +16,6 @@ def find_goal():
 def bfs(i, j):
     q = deque()
     q.append((i, j))
-    visited[i][j] = True
     dist[i][j] = 0
     while q:
         x, y = q.popleft()
@@ -25,19 +23,17 @@ def bfs(i, j):
             nx, ny = x + dx, y + dy
             if not (0 <= nx < n and 0 <= ny < m):
                 continue
-            if matrix[nx][ny] == 0 or visited[nx][ny]:
+            if matrix[nx][ny] == 0 or dist[nx][ny] != -1:
                 continue
             q.append((nx, ny))
-            visited[nx][ny] = True
             dist[nx][ny] = dist[x][y] + 1
 
 def print_output():
     for i in range(n):
-        row = dist[i]
         for j in range(m):
-            if row[j] == float('inf'):
-                row[j] = 0 if matrix[i][j] == 0 else -1
-        print(*row)
+            if matrix[i][j] == 0:
+                dist[i][j] = 0
+        print(*dist[i])
                     
 bfs(*find_goal())
 print_output()
