0953ebf4bfcbf4c98c93889f8d1ba1f472d73f62
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-01-22 19:10:28 +0900

    [Silver I] Title: 회의실 배정, Time: 220 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -6,11 +6,12 @@ N = int(input())
 schedule = [tuple(map(int, input().split())) for _ in range(N)]
 schedule.sort(key=lambda x: (x[1], x[0]))
 
-dp = [0]
+count = 0
+latest = 0
 for start, end in schedule:
-    while len(dp) <= end:
-        dp.append(dp[-1])
-    if dp[end] < dp[start] + 1:
-        dp[end] = dp[start] + 1
-    
-print(dp[-1])
+    if start < latest:
+        continue
+    count += 1
+    latest = end
+
+print(count)
