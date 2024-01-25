b087a420a3023fab22eeedfeeb7fbb03d7f827e5
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-01-25 23:58:36 +0900

    [Gold V] Title: 토마토, Time: 1080 ms, Memory: 134404 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -3,15 +3,17 @@ import sys
 input = sys.stdin.readline
 
 M, N = map(int, input().split())
-tomato = [list(map(int, input().split())) for _ in range(N)]
+tomato = []
 
 q = []
 result = -1
 
 for i in range(N):
+    row = list(map(int, input().split()))
     for j in range(M):
-        if tomato[i][j] == 1:
+        if row[j] == 1:
             q.append((i, j))
+    tomato.append(row)
 
 while q:
     result += 1
