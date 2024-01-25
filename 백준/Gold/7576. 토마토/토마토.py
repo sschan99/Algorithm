15c044ddf607da8220c71b38204bd2adeb692103
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-01-25 23:28:33 +0900

    [Gold V] Title: 토마토, Time: 1840 ms, Memory: 145616 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,34 +1,33 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
 M, N = map(int, input().split())
 tomato = [list(map(int, input().split())) for _ in range(N)]
 
-q = deque()
-result = 0
+q = []
+result = -1
 
 for i in range(N):
     for j in range(M):
         if tomato[i][j] == 1:
-            q.append((i, j, 0))
+            q.append((i, j))
 
 while q:
-    x, y, t = q.popleft()
-    if result < t:
-        result = t
-    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-        nx, ny = x + dx, y + dy
-        if not (0 <= nx < N and 0 <= ny < M):
-            continue
-        if tomato[nx][ny] == 0:
-            tomato[nx][ny] = 1
-            q.append((nx, ny, t + 1))
+    result += 1
+    nq = []
+    for x, y in q:
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            nx, ny = x + dx, y + dy
+            if not (0 <= nx < N and 0 <= ny < M):
+                continue
+            if tomato[nx][ny] == 0:
+                tomato[nx][ny] = 1
+                nq.append((nx, ny))
+    q = nq
 
-for i in range(N):
-    for j in range(M):
-        if tomato[i][j] == 0:
-            print(-1)
-            exit()
+for row in tomato:
+    if 0 in row:
+        print(-1)
+        exit()
 print(result)
