926bc47fd8129020b13c143a96cdb5db6c2be147
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-02-10 15:54:12 +0900

    [Silver III] Title: N과 M (1), Time: 220 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,24 @@
+import sys
+input = sys.stdin.readline
+
 def main():
     n, m = map(int, input().split())
 
     seq = []
+    using = set()
 
     def solve():
         if len(seq) == m:
             print(*seq)
-            
+
         for i in range(1, n + 1):
-            if i in seq:
+            if i in using:
                 continue
             seq.append(i)
+            using.add(i)
             solve()
             seq.pop()
+            using.remove(i)
 
     solve()
 
