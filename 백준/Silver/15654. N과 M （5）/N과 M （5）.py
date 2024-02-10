01d7b8a4f886ebf2215722cc53b881f5610a3a61
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-02-10 16:22:00 +0900

    [Silver III] Title: N과 M (5), Time: 232 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n, m = map(int, input().split())
+    nums = sorted(map(int, input().split()))
+    seq = []
+
+    def solve():
+        if len(seq) == m:
+            print(*seq)
+            return
+
+        for i in nums:
+            if i in seq:
+                continue
+            seq.append(i)
+            solve()
+            seq.pop()
+
+    solve()
+
+if __name__ == '__main__':
+    main()
