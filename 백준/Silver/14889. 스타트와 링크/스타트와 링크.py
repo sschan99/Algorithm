c1f87166c72dceb3b9133f81b3a01658f8b56410
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-02-18 11:22:29 +0900

    [Silver I] Title: 스타트와 링크, Time: 1200 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -3,41 +3,32 @@ input = sys.stdin.readline
 
 def main():
     n = int(input())
-    table = [list(map(int, input().split())) for _ in range(n)]
+    s = [list(map(int, input().split())) for _ in range(n)]
 
-    team_1 = []
-    team_2 = []
-
-    result = []
+    team = []
 
     def cal():
-        temp = 0
-        for i in team_1:
-            for j in team_1:
-                temp += table[i][j]
-        for i in team_2:
-            for j in team_2:
-                temp -= table[i][j]
-        return abs(temp)
+        opponent = [i for i in range(n) if i not in team]
+        result = 0
+        for i in team:
+            for j in team:
+                result += s[i][j]
+        for i in opponent:
+            for j in opponent:
+                result -= s[i][j]
+        return abs(result)
 
-    def re(i):
-        if i == n:
-            result.append(cal())
-            return
+    def combination(x):
+        if len(team) == n // 2:
+            return cal()
+        results = []
+        for i in range(x, n // 2 + len(team)):
+            team.append(i)
+            results.append(combination(i + 1))
+            team.pop()
+        return min(results)
 
-        if len(team_1) < n // 2:
-            team_1.append(i)
-            re(i + 1)
-            team_1.pop()
+    print(combination(0))
 
-        if len(team_2) < n // 2:
-            team_2.append(i)
-            re(i + 1)
-            team_2.pop()
-
-    re(0)
-
-    print(min(result))
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
