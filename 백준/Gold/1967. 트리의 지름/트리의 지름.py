7c837f79f1cafe89bd65f118f915e129121def03
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-02-24 19:02:10 +0900

    [Gold IV] Title: 트리의 지름, Time: 92 ms, Memory: 36988 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10002)
+
+def main():
+    n = int(input())
+    tree = defaultdict(list)
+    for _ in range(n - 1):
+        p, c, w = map(int, input().split())
+        tree[p].append((c, w))
+
+    child_length = defaultdict(list)
+    def recursion(p):
+        if p not in tree:
+            return 0
+        max_length = 0
+        for c, w in tree[p]:
+            x = recursion(c) + w
+            child_length[p].append(x)
+            if max_length < x:
+                max_length = x
+        child_length[p].sort(reverse=True)
+        return max_length
+    recursion(1)
+
+    result = 0
+    for i in range(1, n + 1):
+        if i not in child_length:
+            continue
+        arr = child_length[i]
+        alt = arr[0] + (arr[1] if len(arr) > 1 else 0)
+        if result < alt:
+            result = alt
+    print(result)
+
+if __name__ == '__main__':
+    main()
