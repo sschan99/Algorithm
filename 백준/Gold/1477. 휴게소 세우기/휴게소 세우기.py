18e72d5db2fc1554ef1dc4f0d592cb3028f9827d
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-02-26 19:04:31 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -7,19 +7,15 @@ def main():
     stop = [0] + sorted(map(int, input().split())) + [l]
     road = [stop[i + 1] - stop[i] for i in range(n + 1)]
 
-    def get_need(x):
-        return sum(ceil(r / x) - 1 for r in road)
-
     start, end = 1, l - 1
     while start != end:
         mid = (start + end) // 2
-        if get_need(mid) > m:
-            if start == mid:
-                break
-            start = mid
+        need = sum(ceil(r / mid) - 1 for r in road)
+        if need > m:
+            start = mid + 1
         else:
             end = mid
-    return end
+    return start
 
 if __name__ == '__main__':
     print(main())
