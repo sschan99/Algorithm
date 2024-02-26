ca6b49b5a18dac971274785b24e8da459bbefd77
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-02-26 18:59:27 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -4,28 +4,22 @@ input = sys.stdin.readline
 
 def main():
     n, m, l = map(int, input().split())
-    rest = [0] + sorted(map(int, input().split())) + [l]
-    road = []
-    for i in range(n + 1):
-        road.append(rest[i + 1] - rest[i])
-    road.sort(reverse=True)
-    
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
-    for i in range(n + 1):
-        dp[i][0] = road[0]
+    stop = [0] + sorted(map(int, input().split())) + [l]
+    road = [stop[i + 1] - stop[i] for i in range(n + 1)]
 
-    def recursion(n, m):
-        if n == -1:
-            return 0
-        if not dp[n][m]:
-            candidate = []
-            for i in range(m + 1):
-                alt = max(recursion(n - 1, m - i), ceil(road[n] / (i + 1)))
-                candidate.append(alt)
-            dp[n][m] = min(candidate)
-        return dp[n][m]
-    
-    return(recursion(n, m))
+    def get_need(x):
+        return sum(ceil(r / x) - 1 for r in road)
+
+    start, end = 1, l - 1
+    while start != end:
+        mid = (start + end) // 2
+        if get_need(mid) > m:
+            if start == mid:
+                break
+            start = mid
+        else:
+            end = mid
+    return end
 
 if __name__ == '__main__':
     print(main())
