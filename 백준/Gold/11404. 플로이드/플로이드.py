2045c66284b38f2c559018f4a82d64e6073e7d3c
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-03-09 23:43:16 +0900

    [Gold IV] Title: 플로이드, Time: 228 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -2,25 +2,29 @@ from math import inf
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-m = int(input())
+def main():
+    n = int(input())
+    m = int(input())
 
-dist = [[inf] * n for _ in range(n)]
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    a, b = a - 1, b - 1
-    dist[a][b] = min(dist[a][b], c)
-for i in range(n):
-    dist[i][i] = 0
-
-for k in range(n):
+    dist = [[inf] * n for _ in range(n)]
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        a, b = a - 1, b - 1
+        dist[a][b] = min(dist[a][b], c)
     for i in range(n):
-        for j in range(n):
-            if dist[i][j] > dist[i][k] + dist[k][j]:
-                dist[i][j] = dist[i][k] + dist[k][j]
+        dist[i][i] = 0
 
-for row in dist:
-    for i in range(n):
-        if row[i] == inf:
-            row[i] = 0
-    print(*row)
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    for row in dist:
+        for i in range(n):
+            if row[i] == inf:
+                row[i] = 0
+        print(*row)
+
+if __name__ == '__main__':
+    main()
