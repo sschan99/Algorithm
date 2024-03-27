6326ac2525cd7d4c89cbdc3d52a4bcd4086f84b6
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-03-27 20:55:02 +0900

    [Gold V] Title: 수 나누기 게임, Time: 1100 ms, Memory: 45568 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -4,17 +4,13 @@ input = sys.stdin.readline
 def main():
     n = int(input())
     card = list(map(int, input().split()))
+    max_x = max(card)
     score = {c: 0 for c in card}
     for c in card:
-        for i in range(1, int(c ** 0.5) + 1):
-            if c % i == 0:
-                if i in score:
-                    score[i] += 1
-                    score[c] -= 1
-                j = c // i
-                if i != j and j in score:
-                    score[j] += 1
-                    score[c] -= 1
+        for i in range(c, max_x + 1, c):
+            if i in score:
+                score[c] += 1
+                score[i] -= 1
     print(*[score[c] for c in card])
 
 if __name__ == '__main__':
