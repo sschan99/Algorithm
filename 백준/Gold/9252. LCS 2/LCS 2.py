2a7737010e9fc170fa517133844b3b55932ab5f7
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-03-30 18:50:03 +0900

    [Gold IV] Title: LCS 2, Time: 324 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -11,8 +11,8 @@ def main():
         for j in range(1, m + 1):
             if s1[i - 1] == s2[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                continue
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
     print(dp[n][m])
 
     temp = []
