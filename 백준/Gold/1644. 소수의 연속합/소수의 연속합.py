dc794c147bf38c04484af52e12bcc0c42a628683
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-03-31 11:45:49 +0900

    [Gold III] Title: 소수의 연속합, Time: 5444 ms, Memory: 42592 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n = int(input())
+    if n == 1:
+        return 0
+
+    prime = []
+    for i in range(2, n + 1):
+        limit = int(i ** 0.5)
+        isprime = True
+        for p in prime:
+            if p > limit:
+                break
+            if i % p == 0:
+                isprime = False
+                break
+        if isprime:
+            prime.append(i)
+
+    start = end = len(prime) - 1
+    sum = prime[-1]
+    count = 1 if sum == n else 0
+    while start > 0:
+        start -= 1
+        sum += prime[start]
+        if sum > n:
+            sum -= prime[end]
+            end -= 1
+        if sum == n:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    print(main())
