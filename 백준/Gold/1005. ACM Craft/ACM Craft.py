4d0d877328de90d95817197bcaa1cc3a45686851
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-04-08 23:36:47 +0900

    [Gold III] Title: ACM Craft, Time: 960 ms, Memory: 34324 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,4 +1,3 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -14,15 +13,15 @@ def main():
             count[y] += 1
         w = int(input())
 
-        queue = deque(i for i in range(1, n + 1) if count[i] == 0)
+        stack = [i for i in range(1, n + 1) if count[i] == 0]
         result = d.copy()
-        while queue:
-            u = queue.popleft()
+        while stack:
+            u = stack.pop()
             for v in need[u]:
                 result[v] = max(result[v], result[u] + d[v])
                 count[v] -= 1
                 if count[v] == 0:
-                    queue.append(v)
+                    stack.append(v)
         print(result[w])
 
 if __name__ == '__main__':
