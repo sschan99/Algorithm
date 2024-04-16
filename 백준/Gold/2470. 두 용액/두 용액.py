f85cf3938aa1222cbb7ede606d56532788adbe60
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-04-17 07:08:16 +0900

    [Gold V] Title: 두 용액, Time: 108 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -4,21 +4,20 @@ input = sys.stdin.readline
 def main():
     n = int(input())
     arr = sorted(map(int, input().split()))
-    result = (arr[0], arr[-1])
+    result = (float('inf'), 0, 0)
     left = 0
     right = n - 1
     while left < right:
-        alt = (arr[left], arr[right])
-        s = sum(alt)
-        if abs(sum(result)) > abs(s):
+        alt = (arr[left] + arr[right], arr[left], arr[right])
+        if abs(result[0]) > abs(alt[0]):
             result = alt
-        if s < 0:
+        if alt[0] < 0:
             left += 1
-        elif s > 0:
+        elif alt[0] > 0:
             right -= 1
         else:
             break
-    print(*result)
+    print(result[1], result[2])
 
 if __name__ == '__main__':
     main()
