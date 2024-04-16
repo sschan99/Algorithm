8b41cf60d28313e56bcd3c28937ed9eb9bf40cef
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-04-17 07:54:37 +0900

    [Gold III] Title: 세 용액, Time: 2560 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -4,25 +4,19 @@ input = sys.stdin.readline
 def main():
     n = int(input())
     arr = sorted(map(int, input().split()))
-    result = (0, 0, 0)
-    result_sum = float('inf')
+    result = (float('inf'), 0, 0, 0)
     for i in range(n - 2):
-        num = arr[i]
         left = i + 1
         right = n - 1
         while left < right:
-            alt = (num, arr[left], arr[right])
-            s = sum(alt)
-            if abs(result_sum) > abs(s) and len(set(alt)) == 3:
-                result = alt
-                result_sum = s
+            s = arr[i] + arr[left] + arr[right]
+            if abs(result[0]) > abs(s):
+                result = (s, arr[i], arr[left], arr[right])
             if s < 0:
                 left += 1
-            elif s > 0:
-                right -= 1
             else:
-                break
-    print(*sorted(result))
+                right -= 1
+    print(*result[1:])
 
 if __name__ == '__main__':
     main()
