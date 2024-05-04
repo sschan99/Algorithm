f75accc008c57413ac862cfdba56b76db7aaffde
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-05-04 23:49:57 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 988 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,21 +1,22 @@
 n = int(input())
-result = []
+
+dp = [0]
+for _ in range(n):
+    dp.append(dp[-1] * 2 + 1)
+print(dp[-1])
 
 def hanoi(start, dest, height):
     if height == 1:
-        result.append((start, dest))
-        return 1
+        print(start, dest)
+        return
 
     nums = [1, 2, 3]
     nums.remove(start)
     nums.remove(dest)
     prev_dest = nums.pop()
 
-    x = hanoi(start, prev_dest, height - 1) + 1
-    result.append((start, dest))
-    x += hanoi(prev_dest, dest, height - 1)
-    return x
-
-print(hanoi(1, 3, n))
-for start, dest in result:
+    hanoi(start, prev_dest, height - 1)
     print(start, dest)
+    hanoi(prev_dest, dest, height - 1)
+
+hanoi(1, 3, n)
