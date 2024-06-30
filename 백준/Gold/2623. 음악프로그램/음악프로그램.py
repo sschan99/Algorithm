3a9c714bbc5f0ee865cfa746ccc25b56b9e02eac
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-06-30 12:21:06 +0900

    [Gold III] Title: 음악프로그램, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.py
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.py
@@ -1,36 +1,33 @@
-import heapq
 import sys
 input = sys.stdin.readline
 
-def main():
-    N, M = map(int, input().split())
+N, M = map(int, input().split())
 
-    edge = [[] for _ in range(N + 1)]
-    need = [0] * (N + 1)
+edge = [[] for _ in range(N + 1)]
+need = [0] * (N + 1)
 
-    for _ in range(M):
-        order = list(map(int, input().split()))
-        for i in range(1, order[0]):
-            a, b = order[i], order[i + 1]
-            edge[a].append(b)
-            need[b] += 1
+for _ in range(M):
+    order = list(map(int, input().split()))
+    for i in range(1, order[0]):
+        a, b = order[i], order[i + 1]
+        edge[a].append(b)
+        need[b] += 1
 
-    heap = []
-    for i in range(1, N + 1):
-        heapq.heappush(heap, (need[i], i))
+stack = []
+for i in range(1, N + 1):
+    if need[i] == 0:
+        stack.append(i)
 
-    result = []
-    for _ in range(N):
-        count, current = heapq.heappop(heap)
-        if count != 0:
-            print(0)
-            exit()
-        result.append(current)
-        for num in edge[current]:
-            need[num] -= 1
-            heapq.heappush(heap, (need[num], num))
+result = []
+for _ in range(N):
+    if not stack:
+        print(0)
+        exit()
+    current = stack.pop()
+    result.append(current)
+    for num in edge[current]:
+        need[num] -= 1
+        if need[num] == 0:
+            stack.append(num)
 
-    print(*result, sep='\n')
-
-if __name__ == '__main__':
-    main()
+print(*result, sep='\n')
