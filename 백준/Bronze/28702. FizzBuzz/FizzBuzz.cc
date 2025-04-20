0a2ec28396d4a66754f2c97f0b2f0b5fed8a598e
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2025-04-20 20:05:20 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -4,33 +4,28 @@ using namespace std;
 
 int main()
 {
-	string L1, L2, L3;
-	cin >> L1 >> L2 >> L3;
-
-	int next;
-	if (isdigit(L1[0]))
+	int target = 0;
+	for (int i = 0; i < 3; i++)
 	{
-		next = stoi(L1) + 3;
-	}
-	else if (isdigit(L2[0]))
-	{
-		next = stoi(L2) + 2;
-	}
-	else
-	{
-		next = stoi(L3) + 1;
+		string line;
+		cin >> line;
+		if (isdigit(line[0]))
+		{
+			target = stoi(line) + 3 - i;
+			break;
+		}
 	}
 
-	if (next % 3 != 0 && next % 5 != 0)
+	if (target % 3 != 0 && target % 5 != 0)
 	{
-		cout << next;
+		cout << target;
 		return 0;
 	}
-	if (next % 3 == 0)
+	if (target % 3 == 0)
 	{
 		cout << "Fizz";
 	}
-	if (next % 5 == 0)
+	if (target % 5 == 0)
 	{
 		cout << "Buzz";
 	}
