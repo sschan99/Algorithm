8804b5cd3fbdd6b409f507aae5f78eb5618b317e
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2025-07-23 15:19:23 +0900

    [Gold IV] Title: Count Circle Groups, Time: 1148 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/10216. Count Circle Groups/Count Circle Groups.cc
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.cc
@@ -1,45 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 #define MAX_SIZE 3000
 
 using namespace std;
 
+int N;
+int camp[MAX_SIZE][3];
 bool visited[MAX_SIZE];
 
 void init() {
-	for (int i = 0; i < MAX_SIZE; i++) visited[i] = false;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> camp[i][0] >> camp[i][1] >> camp[i][2];
+		visited[i] = false;
+	}
 }
 
-bool connected(vector<int>& a, vector<int>& b) {
-	int x = b[0] - a[0], y = b[1] - a[1], r = a[2] + b[2];
+bool connected(int a[], int b[]) {
+	int x = b[0] - a[0];
+	int y = b[1] - a[1];
+	int r = a[2] + b[2];
 	return x * x + y * y <= r * r;
 }
 
-void dfs(int x, vector<vector<int>>& data) {
-	for (int i = 0; i < data.size(); i++) {
-		if (visited[i]) continue;
-		if (!connected(data[x], data[i])) continue;
-		visited[i] = true;
-		dfs(i, data);
+void dfs(int x) {
+	for (int i = 0; i < N; i++) {
+		if (!visited[i] && connected(camp[x], camp[i])) {
+			visited[i] = true;
+			dfs(i);
+		}
 	}
 }
 
 void cal() {
-	int N, count = 0;
-	cin >> N;
-	vector<vector<int>> data(N, vector<int>(3));
-	for (auto& camp : data) cin >> camp[0] >> camp[1] >> camp[2];
-
 	init();
+	int count = 0;
+
 	for (int i = 0; i < N; i++) {
-		if (visited[i]) continue;
-		visited[i] = true;
-		dfs(i, data);
-		count++;
+		if (!visited[i]) {
+			visited[i] = true;
+			dfs(i);
+			++count;
+		}
 	}
-	cout << count << endl;;
+	cout << count << '\n';
 }
 
 int main() {
@@ -48,6 +53,6 @@ int main() {
 
 	int T;
 	cin >> T;
-	for (int i = 0; i < T; i++) cal();
+	while (T--) cal();
 	return 0;
 }
