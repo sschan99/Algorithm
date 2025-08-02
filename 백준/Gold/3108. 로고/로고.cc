a776d143bbdb8ed13df3669924ea639515fdbb20
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2025-08-02 18:10:14 +0900

    UnionFind Class

--- 백준/Gold/3108. 로고/로고.cc
+++ 백준/Gold/3108. 로고/로고.cc
@@ -3,19 +3,39 @@
 
 using namespace std;
 
+class UnionFind {
+private:
+    //vector<int> rank;
+    vector<int> root;
+    
+public:
+    UnionFind(int N) {
+        //rank.resize(N, 1);
+        root.resize(N);
+        for (int i = 0; i < N; i++) {
+		    root[i] = i;
+	    }
+    }
+    
+    int find(int x) {
+	    if (root[x] == x) return x;
+	    return root[x] = find(root[x]);
+    }
+    
+    void merge(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+        if (rx == ry) return;
+        //if (rank[rx] == rank[ry]) rank[rx]++;
+        //else if (rank[rx] < rank[ry]) swap(rx, ry);
+        root[ry] = rx;
+    }
+};
+
 static bool divided(vector<int>& a, vector<int>& b) {
 	return (a[2] < b[0] || b[2] < a[0] || a[3] < b[1] || b[3] < a[1])
 		|| (a[0] < b[0] && a[1] < b[1] && a[2] > b[2] && a[3] > b[3])
 		|| (a[0] > b[0] && a[1] > b[1] && a[2] < b[2] && a[3] < b[3]);
-}
-
-static int find(vector<int>& root, int x) {
-	if (root[x] == x) return x;
-	return root[x] = find(root, root[x]);
-}
-
-static void merge(vector<int>& root, int x, int y) {
-	root[find(root, x)] = find(root, y);
 }
 
 static void solve() {
@@ -32,22 +52,19 @@ static void solve() {
 		}
 	}
 
-	vector<int> root(N);
-	for (int i = 0; i < N; i++) {
-		root[i] = i;
-	}
-
+    UnionFind UF(N);
+    
 	for (int i = 0; i < N - 1; i++) {
 		for (int j = i + 1; j < N; j++) {
 			if (!divided(data[i], data[j])) {
-				merge(root, i, j);
+				UF.merge(i, j);
 			}
 		}
 	}
 	
 	int count = -1;
 	for (int i = 0; i < N; i++) {
-		if (find(root, i) == i) count++;
+		if (UF.find(i) == i) count++;
 	}
 	cout << count;
 }
